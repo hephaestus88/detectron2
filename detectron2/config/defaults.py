0c1ff8bbfcd026dbcbf920cc0e429696be6311b6
hephaestus88 <103326480+hephaestus88@users.noreply.github.com>
2023-03-23 11:29:21 -0400

    add cbam config

--- detectron2/config/defaults.py
+++ detectron2/config/defaults.py
@@ -136,6 +136,8 @@ _C.MODEL.BACKBONE.NAME = "build_resnet_backbone"
 # stages are each group of residual blocks.
 _C.MODEL.BACKBONE.FREEZE_AT = 2
 
+# use which attention modules, options: cbam
+_C.MODEL.BACKBONE.ATTENTION_MODULE = ""
 
 # ---------------------------------------------------------------------------- #
 # FPN options
