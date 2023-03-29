9afb7f71b46890a67eeb0215697fbe842d55a04f
hephaestus88 <103326480+hephaestus88@users.noreply.github.com>
2023-03-29 11:04:22 -0400

    Update coco.py

--- configs/common/data/coco.py
+++ configs/common/data/coco.py
@@ -32,7 +32,7 @@ dataloader.train = L(build_detection_train_loader)(
 )
 
 dataloader.test = L(build_detection_test_loader)(
-    dataset=L(get_detection_dataset_dicts)(names="defect_val", filter_empty=False),
+    dataset=L(get_detection_dataset_dicts)(names="defect_test", filter_empty=False),
     mapper=L(DatasetMapper)(
         is_train=False,
         augmentations=[
