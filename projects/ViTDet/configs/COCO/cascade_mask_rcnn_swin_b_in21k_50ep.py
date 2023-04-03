c29bc6f60f557e4a2e8b31c500e4b407da39b0a9
hephaestus88 <103326480+hephaestus88@users.noreply.github.com>
2023-04-03 11:17:53 -0400

    Update cascade_mask_rcnn_swin_b_in21k_50ep.py

--- projects/ViTDet/configs/COCO/cascade_mask_rcnn_swin_b_in21k_50ep.py
+++ projects/ViTDet/configs/COCO/cascade_mask_rcnn_swin_b_in21k_50ep.py
@@ -29,7 +29,7 @@ train.max_iter = 3000
 lr_multiplier = L(WarmupParamScheduler)(
     scheduler=L(MultiStepParamScheduler)(
         values=[1.0, 0.1, 0.01],
-        milestones=[163889, 177546],
+        milestones=[1000, 2000],
         num_updates=train.max_iter,
     ),
     warmup_length=250 / train.max_iter,
