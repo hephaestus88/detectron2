e86a01c3ad6a6f69f07ad23f47a569f55892d5b0
hephaestus88 <103326480+hephaestus88@users.noreply.github.com>
2023-04-03 23:08:57 -0400

    Update train.py

--- configs/common/train.py
+++ configs/common/train.py
@@ -3,15 +3,15 @@
 train = dict(
     output_dir="./output",
     init_checkpoint="",
-    max_iter=90000,
+    max_iter=3000,
     amp=dict(enabled=False),  # options for Automatic Mixed Precision
     ddp=dict(  # options for DistributedDataParallel
         broadcast_buffers=False,
         find_unused_parameters=False,
         fp16_compression=False,
     ),
-    checkpointer=dict(period=5000, max_to_keep=100),  # options for PeriodicCheckpointer
-    eval_period=5000,
+    checkpointer=dict(period=200, max_to_keep=100),  # options for PeriodicCheckpointer
+    eval_period=1000,
     log_period=20,
     device="cuda"
     # ...
